60fc942af2c0db4258bd49f284aace3a8701d623
n0m3l4c000nt35 <zar23001@byui.edu>
2024-09-18 21:04:35 -0300

    W02: Team Activity - Home Page Enhancement

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,7 +1,36 @@
 const $year = document.querySelector("#currentyear");
 const $lastModified = document.querySelector("#lastModified");
+const $btnBurguerMenu = document.querySelector("#btnBurguerMenu");
+const $menuLinks = document.querySelector("#animated");
+const $modeButton = document.querySelector("#mode");
+const $main = document.querySelector("main");
 
 const today = new Date();
 
 $year.innerHTML = today.getFullYear();
 $lastModified.innerHTML = `Last Modification: ${document.lastModified}`;
+
+$btnBurguerMenu.addEventListener("click", () => {
+  $menuLinks.classList.toggle("open");
+  $btnBurguerMenu.classList.toggle("open");
+});
+
+$modeButton.addEventListener("click", () => {
+  if ($modeButton.textContent.includes("☀️")) {
+    document.body.style.backgroundColor = "#fff";
+    $main.style.background = "#fff";
+    $main.style.color = "#000";
+    $main.querySelector("h1").style.color = "#3b3b3b";
+    $main.querySelectorAll("li").forEach(el => (el.style.color = "#000"));
+    $main.querySelectorAll("a").forEach(el => (el.style.color = "#000"));
+    $modeButton.textContent = "🌙";
+  } else {
+    document.body.style.backgroundColor = "#1a1a1a";
+    $main.style.backgroundColor = "#1a1a1a";
+    $main.querySelector("h1").style.color = "#fff";
+    $main.querySelectorAll("li").forEach(el => (el.style.color = "#fff"));
+    $main.querySelectorAll("a").forEach(el => (el.style.color = "#fff"));
+    $main.style.color = "#fff";
+    $modeButton.textContent = "☀️";
+  }
+});
