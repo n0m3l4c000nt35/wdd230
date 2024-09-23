7aaead81ddde49101a723a12596cac8bf320e03c
n0m3l4c000nt35 <zar23001@byui.edu>
2024-09-23 13:00:36 -0300

    Web Storage API - localStorage

---
+++ week03/scripts/bom.js
@@ -0,0 +1,44 @@
+const $input = document.querySelector("#favchap");
+const $button = document.querySelector("button");
+const $list = document.querySelector("#list");
+
+$button.addEventListener("click", () => {
+  if ($input.value != "") {
+    displayList($input.value);
+    chaptersArray.push($input.value);
+    setChapterList();
+    $input.value = "";
+    $input.focus();
+  }
+  alert("Enter Book and Chapter!");
+  $input.focus();
+});
+
+function displayList(item) {
+  let $li = document.createElement("li");
+  let $deletebutton = document.createElement("button");
+  $li.textContent = item;
+  $deletebutton.textContent = "❌";
+  $deletebutton.classList.add("delete");
+  $li.append($deletebutton);
+  $list.append($li);
+  $deletebutton.addEventListener("click", () => {
+    $list.removeChild($li);
+    deleteChapter($li.textContent);
+    $input.focus();
+  });
+}
+
+const setChapterList = () =>
+  localStorage.setItem("myFavBOMList", JSON.stringify(chaptersArray));
+
+const getChapterList = () => JSON.parse(localStorage.getItem("myFavBOMList"));
+
+function deleteChapter(chapter) {
+  chapter = chapter.slice(0, chapter.length - 1);
+  chaptersArray = chaptersArray.filter(item => item !== chapter);
+  setChapterList();
+}
+
+let chaptersArray = getChapterList() || [];
+chaptersArray.forEach(chapter => displayList(chapter));
