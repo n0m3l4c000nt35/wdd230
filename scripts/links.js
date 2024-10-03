6b9758527f3337a81902838974ef5cca43db7a7d
n0m3l4c000nt35 <zar23001@byui.edu>
2024-10-03 14:24:57 -0300

    W05: Team Activity - JSON and the Weather API

--- scripts/links.js
+++ scripts/links.js
@@ -4,13 +4,29 @@ const baseURL = "https://n0m3l4c000nt35.github.io/wdd230/";
 const linksURL = "https://n0m3l4c000nt35.github.io/wdd230/data/links.json";
 
 function displayLinks(weeks) {
-  console.log(weeks);
+  weeks.forEach(({ week, links }) => {
+    const li = document.createElement("li");
+    const b = document.createElement("b");
+    b.textContent = `${week}: `;
+    li.appendChild(b);
+    links.forEach(({ title, url }, index) => {
+      const a = document.createElement("a");
+      a.textContent = title;
+      a.setAttribute("href", url);
+      a.setAttribute("target", "_blank");
+      li.appendChild(a);
+      if (index !== links.length - 1) {
+        a.insertAdjacentText("afterend", " | ");
+      }
+    });
+    weekList.appendChild(li);
+  });
 }
 
 async function getLinks() {
   const response = await fetch(linksURL);
-  const data = await response.json();
-  displayLinks(data);
+  const { weeks } = await response.json();
+  displayLinks(weeks);
 }
 
 getLinks();
