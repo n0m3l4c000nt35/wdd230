5e2bbf0e1dda1405f141615b8e1af0e48975f3ee
n0m3l4c000nt35 <zar23001@byui.edu>
2024-10-03 13:40:44 -0300

    update links,json

--- scripts/links.js
+++ scripts/links.js
@@ -1,10 +1,16 @@
+const weekList = document.querySelector("#weekList");
+
 const baseURL = "https://n0m3l4c000nt35.github.io/wdd230/";
 const linksURL = "https://n0m3l4c000nt35.github.io/wdd230/data/links.json";
+
+function displayLinks(weeks) {
+  console.log(weeks);
+}
 
 async function getLinks() {
   const response = await fetch(linksURL);
   const data = await response.json();
-  console.log(data);
+  displayLinks(data);
 }
 
 getLinks();
