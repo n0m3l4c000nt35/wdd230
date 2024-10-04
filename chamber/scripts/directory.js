e9a7fb03cdc9f14a015258855b16bf0c99919117
n0m3l4c000nt35 <zar23001@byui.edu>
2024-10-03 21:39:10 -0300

    update

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,4 +1,6 @@
 const main = document.querySelector("#directory");
+const gridViewButton = document.querySelector("#gridViewButton");
+const listViewButton = document.querySelector("#listViewButton");
 
 const urlMembers = "https://n0m3l4c000nt35.github.io/wdd230/chamber/data/members.json";
 
@@ -23,5 +25,14 @@ const fetchMembers = async () => {
   const { members } = await response.json();
   displayMembers(members);
 };
+
+gridViewButton.addEventListener("click", () => {
+  main.classList.remove("directoryListView");
+  main.classList.add("directoryGridView");
+});
+listViewButton.addEventListener("click", () => {
+  main.classList.remove("directoryGridView");
+  main.classList.add("directoryListView");
+});
 
 fetchMembers();
